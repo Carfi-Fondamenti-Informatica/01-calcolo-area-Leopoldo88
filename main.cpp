3e4d6b9eeba5a4fd2c47164fc85fb6050381cf13
Leopoldo88 <114876690+Leopoldo88@users.noreply.github.com>
2022-11-10 20:20:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,24 @@
 using namespace std;
 
 int main(){
+
+float a=0;
+float b=0
+float c=0
+  
+  
 //Qui vanno definite le variabili
 
 cin >> a >> b >> c;
 
+
+
 //Qui va scritto il codice per eseguire i calcoli richiesti
+cout << (a*b)/2 << endl;
+cout << (a*a) << endl;
+cout << (a*b) << endl;
+cout << ((a+b)*c)/2 << endl;
+
 
 
 }
